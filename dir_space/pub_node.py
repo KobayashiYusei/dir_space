a90189c4c87ae4301a412a488c42f0d20a2ce04a
KobayashiYusei <s23C1054AF@s.chibakoudai.jp>
2025-01-04 16:58:21 +0900

    Add command

--- dir_space/pub_node.py
+++ dir_space/pub_node.py
@@ -1,18 +1,25 @@
 import os
 import shutil
+import sys
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float64
 
 class diskSpace(Node):
-    def __init__(self):
+    def __init__(self, path):
         super().__init__('disk_space')
         self.pub = self.create_publisher(Float64, 'disk_space', 10)
         self.timer = self.create_timer(1.0, self.cb)
+        self.path = path
+        if not os.path.exists(self.path):
+            self.get_logger().error(f"path does not exist: {self.path}")
+            self.path = '/' 
+            sys.exit(1)
+        self.get_logger().info(f"Publish disk space of{self.path}")
 
     def cb(self):
         try:
-            total, used, free = shutil.disk_usage('/')
+            total, used, free = shutil.disk_usage(self.path)
             free_gb = free / (2**30)
             usage = used/total * 100
             msg = Float64()
@@ -24,7 +31,11 @@ class diskSpace(Node):
 
 def main(args=None):
     rclpy.init(args=args)
-    node = diskSpace()
+    if len(sys.argv) > 1:
+        path = sys.argv[1]
+    else:
+        path = '/'
+    node = diskSpace(path)
     try:
         rclpy.spin(node)
     except KeyboardInterrupt:
