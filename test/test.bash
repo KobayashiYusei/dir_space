5be99b83245ae25b07aaf89660664bfa39dc6d53
KobayashiYusei <s23C1054AF@s.chibakoudai.jp>
2025-01-04 18:53:05 +0900

    Add test

---
+++ test/test.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+dir=~
+[ "$1 != "" ] && dir = "$1"
+
+cd $dir/ros2_hm
+colcon build
+source $dir/.bashrc
+timeout ros2 run dir_space pub_node /mnt/wsl > /tmp/dir_space.log
+
+cat /tmp/dir_space.log | grep 'Free disk space'
+
