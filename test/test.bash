7a905fd9aaf1a5f4f707eeba70342aaf6f4e9269
KobayashiYusei <s23C1054AF@s.chibakoudai.jp>
2025-01-04 19:32:49 +0900

    Modify test

--- test/test.bash
+++ test/test.bash
@@ -5,6 +5,5 @@ dir=~
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-timeout ros2 run dir_space pub_node / > /tmp/dir_space.log
-
+timeout 10 ros2 run dir_space pub_node / > /tmp/dir_space.log
 cat /tmp/dir_space.log | grep 'Free disk space'
