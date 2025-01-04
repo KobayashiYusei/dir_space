ec943175b8dab06bb21b62f244ce93192de717df
KobayashiYusei <s23C1054AF@s.chibakoudai.jp>
2025-01-04 16:10:22 +0900

    Add command

--- dir_space/pub_node.py
+++ dir_space/pub_node.py
@@ -4,28 +4,27 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float64
 
-class DiskSpacePublisher(Node):
+class diskSpace(Node):
     def __init__(self):
-        super().__init__('disk_space_publisher')
-        self.publisher_ = self.create_publisher(Float64, 'disk_space', 10)
-        self.timer = self.create_timer(1.0, self.timer_callback)  # Publish every second
-        self.directory = '/'
-        self.get_logger().info(f"Publishing disk space for directory: {self.directory}")
+        super().__init__('disk_space')
+        self.pub = self.create_publisher(Float64, 'disk_space', 10)
+        self.timer = self.create_timer(1.0, self.cb)
 
-    def timer_callback(self):
+    def cb(self):
         try:
-            total, used, free = shutil.disk_usage(self.directory)
-            free_gb = free / (1024**3)  # Convert bytes to gigabytes
+            total, used, free = shutil.disk_usage('/')
+            free_gb = free / (2**30)
+            usage = used/total * 100
             msg = Float64()
-            msg.data = float(free_gb)  # Ensure the value is a float
-            self.publisher_.publish(msg)
-            self.get_logger().info(f"Free disk space: {free_gb:.2f} GB")
+            msg.data = float(free_gb)
+            self.pub.publish(msg)
+            self.get_logger().info(f"Free disk space: {free_gb:.2f} GB Usage:{usage:.2f}%")
         except Exception as e:
             self.get_logger().error(f"Failed to calculate disk space: {e}")
 
 def main(args=None):
     rclpy.init(args=args)
-    node = DiskSpacePublisher()
+    node = diskSpace()
     try:
         rclpy.spin(node)
     except KeyboardInterrupt:
