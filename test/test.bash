7379925accf1092b217234deb508c14940ad7338
KobayashiYusei <s23C1054AF@s.chibakoudai.jp>
2025-01-05 15:44:47 +0900

    Modify code

--- test/test.bash
+++ test/test.bash
@@ -5,8 +5,7 @@ dir=~
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-timeout 10 ros2 run dir_space pub_node / > test_valid.log 2>&1 &
-VALID_PID=$!
+ros2 run dir_space pub_node / > test_valid.log 2>&1 & VALID_PID=$!
 sleep 3
 kill $VALID_PID
 if grep -q "Publish disk space of /" test_valid.log; then
@@ -15,5 +14,3 @@ else
   echo "Valid path test failed. Check test_valid.log."
   exit 1
 fi
-
-ros2 topic echo /dir_space
